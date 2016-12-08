8745d0c8a8da907a7f6fd085d2185f9b3d46c9cc
dharrell96 <dharrell96@gmail.com>
2016-12-08 12:56:46 -0500

    Closer to working version

--- mockup/php/orm/Tournament.php
+++ mockup/php/orm/Tournament.php
@@ -73,10 +73,10 @@ class Tournament
                 $start_date = null;
             }
 
-            if($tournament_info['start_date'] != null){
-                $start_date = new DateTime($tournament_info['start_date']);
+            if($tournament_info['end_date'] != null){
+                $end_date = new DateTime($tournament_info['end_date']);
             }else{
-                $start_date = null;
+                $end_date = null;
             }
 
             return new Tournament($tournament_info['id'], $tournament_info['name'], $tournament_info['league_id'], $tournament_info['num_teams'], $tournament_info['start_date'], $tournament_info['end_date']);
@@ -117,7 +117,7 @@ class Tournament
 
     public function getNumTeams()
     {
-        return this->$this->num_teams;
+        return $this->num_teams;
     }
 
 
@@ -134,30 +134,46 @@ class Tournament
     public function setName($name)
     {
         $this->name = $name;
+        return $this->update();
     }
 
     public function setLeagueID($league_id)
     {
         $this->league_id = $league_id;
+        return $this->update();
     }
 
     public function setNumTeams($num_teams)
     {
         $this->num_teams = $num_teams;
+        return $this->update();
     }
 
     public function setStartDate($start_date)
     {
         $this->start_date = $start_date;
+        return $this->update();
     }
 
     public function setEndDate($end_date)
     {
         $this->end_date = $end_date;
+        return $this->update();
     }
 
     private function update(){
         $mysqli = Tournament::connect();
+        if($this->start_date==null){
+            $sdstr= "null";
+        }else{
+            $sdstr="'" . $this->start_date->format('Y-m-d') . "'";
+        }
+        if($this->end_date==null){
+            $edstr= "null";
+        }else{
+            $edstr="'" . $this->end_date->format('Y-m-d') . "'";
+        }
+
         $result = $mysqli->query("UPDATE tournament set " .
             "name=" .
             "'" . $mysqli->real_escape_string($this->name) . "', " .
@@ -166,24 +182,26 @@ class Tournament
             "num_teams=" .
             "'" . $this->num_teams . "', " .
             "start_date=" .
-            "'" . $mysqli->real_escape_string($this->email) . "', " .
+            "'" . $sdstr . "', " .
             "end_date=" .
-            "'" . $mysqli->real_escape_string($this->password) . "' WHERE id=" . $this->id);
+            "'" . $edstr . "' WHERE id=" . $this->id);
 
-        return result;
+        return $result;
 
 
     }
     public function delete() {
-        $mysqli = Tournament::connec();
+        $mysqli = Tournament::connect();
         $mysqli->query("DELETE FROM tournament WHERE id=" . $this->id);
     }
 
     public function getJSON()
     {
         $json_obj = array('id' => $this->userid,
-            'email' => $this->email,
-            'password' => $this->password);
+            'name' => $this->name,
+            'num_teams' => $this->num_teams,
+            'start_date'=>$this->start_date,
+            'end_date'=>$this->end_date);
         return json_encode($json_obj);
     }
 
