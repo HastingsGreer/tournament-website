79a7e8928999c32ae188244105a7f6ba0c8cb2a8
Thomas Hastings Greer <tgreer@classroom.cs.unc.edu>
2016-12-08 16:02:28 -0500

    endpoints mockup

---
+++ mockup/getTournament.php
@@ -0,0 +1,57 @@
+<?php
+header('Content-Type: application/json');
+$id = $_GET['id'];
+$teams = array();
+$teamnames = ['Broncos', 'Bulls', 'Wingnuts', 'Gamecocks', 'Ampersands', 'xss', 'tar heels', 'snoot boopers', 'fly balls', 'team chaos', 'cow tippers', 'crazy 8s', 'ballers', 'robots', 'cat-people', 'brainsuckers'];
+$i = 1000;
+foreach($teamnames as $name) {
+    $teams[] = (object) array("team_name" => $name,
+                              "team_id" => $i,
+			      "team_league" => $i%4,
+			      "seed" => null);
+    $i += 1;
+}
+$leagues = array();
+foreach([1, 2, 3, 4] as $leagueid) {
+    $league = array("league_id" => $leagueid);
+    $lteams = array();
+    foreach($teams as $t){
+        if($t->team_league == $leagueid){
+	    $lteams[] = $t$team_id];
+	}
+    }
+    $league["teams_in_league"] = $lteams;
+    $leagues[] = $league;
+
+}
+
+$games = (object) array("leagues" => $leagues);
+
+$tournament1 = (object) array(
+                              'numteams'=> 16,
+                              'numleagues'=> 4,
+                              'in_bracket_play' => false,
+                              'tournament_style' => 'round_robin',
+			      'teams' => $teams,
+			      'games' => $games,
+			      );
+
+
+
+
+$tournament2 = (object) array('tournament' => 'UNC basketball Intramurals 2016',
+                              'tournament_id' => '1235',
+                              'tournament_day' => '12-23-16');
+
+if($id == 1234){
+    echo json_encode($tournament1);
+} else {
+    if($id == 1235){
+        echo json_encode($tournament2);
+    } else {
+        echo json_encode(null);
+    }
+}
+?>
+
+
