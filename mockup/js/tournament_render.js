98d8c9f258e539a327a0dd7c40c105f28aed877e
dharrell96 <dharrell96@gmail.com>
2016-12-09 10:20:46 -0500

    a

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
 		var teams = json.teams;
 		var games = json.games;
 		var bracket = json.games.bracket;
-		alert(bracket);
 		var id2name = {}
 		for(var idx = 0; idx < teams.length; idx++){
 			id2name[teams[idx].team_id] = teams[idx];
@@ -95,14 +94,85 @@ $(document).ready(function(){
 
 			var bracketpos = game.bracket_position;
 			alert(bracketpos);
-			if(bracketpos==7){
-				alert("Should see a change.");
+			if(bracketpos==0){
+
+				document.getElementById("0home").innerHTML=t1seed + " " + t1;
+				document.getElementById("0away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("0hscore").innerHTML=t1s;
+				document.getElementById("0ascore").innerHTML=t2s;
+			}else if(bracketpos==1){
+
+				document.getElementById("1home").innerHTML=t1seed + " " + t1;
+				document.getElementById("1away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("1hscore").innerHTML=t1s;
+				document.getElementById("1ascore").innerHTML=t2s;
+			}else if(bracketpos==2){
+				document.getElementById("2home").innerHTML=t1seed + " " + t1;
+				document.getElementById("2away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("2hscore").innerHTML=t1s;
+				document.getElementById("2ascore").innerHTML=t2s;
+			}else if(bracketpos==3){
+
+				document.getElementById("3home").innerHTML=t1seed + " " + t1;
+				document.getElementById("3away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("3hscore").innerHTML=t1s;
+				document.getElementById("3ascore").innerHTML=t2s;
+			}else if(bracketpos==4){
+				document.getElementById("4home").innerHTML=t1seed + " " + t1;
+				document.getElementById("4away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("4hscore").innerHTML=t1s;
+				document.getElementById("4ascore").innerHTML=t2s;
+			}else if(bracketpos==5){
+				document.getElementById("5home").innerHTML=t1seed + " " + t1;
+				document.getElementById("5away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("5hscore").innerHTML=t1s;
+				document.getElementById("5ascore").innerHTML=t2s;
+			}else if(bracketpos==6){
+				document.getElementById("6home").innerHTML=t1seed + " " + t1;
+				document.getElementById("6away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("6hscore").innerHTML=t1s;
+				document.getElementById("6ascore").innerHTML=t2s;
+			}else if(bracketpos==7){
 				document.getElementById("7home").innerHTML=t1seed + " " + t1;
 				document.getElementById("7away").innerHTML=t2seed+ " " + t2;
 				document.getElementById("7hscore").innerHTML=t1s;
 				document.getElementById("7ascore").innerHTML=t2s;
+			}else if(bracketpos==8){
+				document.getElementById("8home").innerHTML=t1seed + " " + t1;
+				document.getElementById("8away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("8hscore").innerHTML=t1s;
+				document.getElementById("8ascore").innerHTML=t2s;
+			}else if(bracketpos==9){
+				document.getElementById("9home").innerHTML=t1seed + " " + t1;
+				document.getElementById("9away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("9hscore").innerHTML=t1s;
+				document.getElementById("9ascore").innerHTML=t2s;
+			}else if(bracketpos==10){
+				document.getElementById("10home").innerHTML=t1seed + " " + t1;
+				document.getElementById("10away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("10hscore").innerHTML=t1s;
+				document.getElementById("10ascore").innerHTML=t2s;
+			}else if(bracketpos==11){
+				document.getElementById("11home").innerHTML=t1seed + " " + t1;
+				document.getElementById("11away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("11hscore").innerHTML=t1s;
+				document.getElementById("11ascore").innerHTML=t2s;
+			}else if(bracketpos==12){
+				document.getElementById("12home").innerHTML=t1seed + " " + t1;
+				document.getElementById("12away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("12hscore").innerHTML=t1s;
+				document.getElementById("12ascore").innerHTML=t2s;
+			}else if(bracketpos==13){
+				document.getElementById("13home").innerHTML=t1seed + " " + t1;
+				document.getElementById("13away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("13hscore").innerHTML=t1s;
+				document.getElementById("13ascore").innerHTML=t2s;
+			}else if(bracketpos==14){
+				document.getElementById("14home").innerHTML=t1seed + " " + t1;
+				document.getElementById("14away").innerHTML=t2seed+ " " + t2;
+				document.getElementById("14hscore").innerHTML=t1s;
+				document.getElementById("14ascore").innerHTML=t2s;
 			}
-
 		}
 		
 
