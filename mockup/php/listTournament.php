4ff78f37b77abe02d366b0c003bab25090352c5f
dharrell96 <dharrell96@gmail.com>
2016-12-09 00:53:38 -0500

    More changes

--- mockup/php/listTournament.php
+++ mockup/php/listTournament.php
@@ -22,9 +22,7 @@ if (isset($_REQUEST['userid'])) {
 }else{
 	echo "Made it into listtournament";
 	$sql = "SELECT * FROM tournament";
-	echo $sql;
 	$result = $conn->query($sql);
-	echo $result->num_rows;
 	if ($result->num_rows > 0) {
     // output data of each row
 		while($row = $result->fetch_assoc()) {
