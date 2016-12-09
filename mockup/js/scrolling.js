3a89cefb086732d78cf51eb8b1f7ea352309e5f0
kham131 <kham131@gmail.com>
2016-12-09 05:43:23 -0500

    Create scrolling.js

---
+++ mockup/js/scrolling.js
@@ -0,0 +1,10 @@
+
+    $(document).ready(function (){
+    $(document).on('click', 'a', function(event){
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 800);
+    });
+})
