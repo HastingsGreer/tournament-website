247991b1047ed7e37cb1e9080b746ad650f54225
dharrell96 <dharrell96@gmail.com>
2016-12-09 07:19:07 -0500

    adf

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
 				a1s.innerHTML=t2score;
 				tr.appendChild(th);
 
-				document.getElementById("pool_container").innerHTML=tr;
+				document.getElementById("pool_container").appendChild(tr.innerHTML);
 				
 			}
 		}
