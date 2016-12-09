762ca220f4afc01de59706987d641f10a5e85284
dharrell96 <dharrell96@gmail.com>
2016-12-09 06:21:50 -0500

    ssasdf

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -12,7 +12,9 @@ $(document).ready(function(){
 		var teams = json.teams;
 		var games = json.games;
 		for(var leagues=0; leagues< games.length; leagues++){
+			alert(leagues);
 			for(var g=0; g<games['leagues'].length; g++){
+				alert(g);
 				alert(games[leagues][g].league_id);
 			}
 		}
