fb6e53fc007ca89292e1414a18f451514b0d6e4f
Thomas Hastings Greer <tgreer@classroom.cs.unc.edu>
2016-12-08 22:15:02 -0500

    horse

--- mockup/makeTournament.php
+++ mockup/makeTournament.php
@@ -1,38 +1,69 @@
 <?php
 $json = file_get_contents('php://input'); $obj = json_decode($json);
-
+echo "\n" . $json . "\n";
+$mysqli = mysqli_connect("classroom.cs.unc.edu", "tgreer", "horsejump5678", "tgreerdb");
 echo "d". $obj->tournament_style . "77\n";
 
-$id = intval(1234);
+
+mysqli_query($mysqli, "INSERT INTO tournament (end_date, is_in_bracket_play, min_rest_for_team, name, num_pools, num_teams, start_date) VALUES ( '" . $obj->end_date . "',false,'" . $obj->min_rest_time ."','". $obj->name  . "','" . $obj->numleagues. "','" .$obj->numteams . "','" .$obj->start_date."')");
+echo "INSERT INTO tournament (end_date, is_in_bracket_play, min_rest_for_team, name, num_pools, num_teams, start_date) VALUES ( '" . $obj->end_date . "',false,'" . $obj->min_rest_time ."','". $obj->name  . "','" . $obj->numleagues. "','" .$obj->numteams . "','" .$obj->start_date."')";
+
+
+$tourneyID = mysqli_query($mysqli, "SELECT id FROM tournament WHERE name = '" . $obj->name . "'")->fetch_row()[0];
+echo "dhi\n" . mysqli_error($mysqli)."\n horse";
+echo $tourneyID . "\n";
+
+
+
+$leagues = array();
+for($i = 0; $i < $obj->numleagues; $i = $i + 1){
+    $name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[$i];    
+    mysqli_query($mysqli, "INSERT INTO pool (name, tournamentID) VALUES ( '" . $name . "','". $tourneyID . "')");
+    $leagues[] = mysqli_query($mysqli, "SELECT ID FROM pool WHERE name = '" . $name . "' AND tournamentID = " . $tourneyID)->fetch_row()[0];
+    echo "id: ".$leagues[$i]."\n";
+
+    echo "dhi\n" . mysqli_error($mysqli)."\n horse";
+}
+
 $teams = array();
-$teamnames = ['Broncos', 'Bulls', 'Wingnuts', 'Gamecocks', 'Ampersands', 'xss', 'tar heels', 'snoot boopers', 'fly balls', 'team chaos', 'cow tippers', 'crazy 8s', 'ballers', 'robots', 'cat-people', 'brainsuckers'];
 $i = 1000;
-foreach($teamnames as $name) {
-    $teams[] = (object) array("team_name" => $name,
-                              "team_id" => $i,
-			      "team_league" => $i%4,
+foreach($obj->teams as $name) {
+    mysqli_query($mysqli, "INSERT INTO team (name, poolID, seed, tournamentID) VALUES ( '" . $name . "','" . $leagues[$i % $obj->numleagues] . "','".null. "','". $tourneyID . "')");
+    echo $name . "\n";
+    $teams[] =  (object) array(
+                              "id" => mysqli_query($mysqli, "SELECT id FROM team WHERE name = '" . $name . "' AND tournamentID = " . $tourneyID)->fetch_row()[0],
+			      "team_league" => $leagues[$i%$obj->numleagues],
 			      "seed" => null);
+    
     $i += 1;
 }
-$leagues = array();
-foreach([0, 1, 2, 3] as $leagueid) {
-    $league = array("league_id" => $leagueid);
+#var_dump($leagues);
+#var_dump($teams);
+echo "generate league games\n";
+foreach($leagues as $league) {
+    echo $league . "\n";
     $lteams = array();
     foreach($teams as $t){
-        if($t->team_league == $leagueid){
-	    $lteams[] = $t->team_id;
+        if($t->team_league == $league){
+	    $lteams[] = $t->id;
+            #echo $t->id;
 	}
     }
-    $league["teams_in_league"] = $lteams;
     foreach($lteams as $id1) {
+       foreach($lteams as $id2){
+           if(intval($id1) < intval($id2)){
+               echo $id1 . $id2 . "\n";
+                
+               mysqli_query($mysqli, "INSERT INTO game (name, tournamentID) VALUES ( '" . $name . "','". $tourneyID . "')");
+           }    
+       }
     }
-    $leagues[] = $league;
+    
 
 }
 
 $games = (object) array("leagues" => $leagues,
                         "bracket" => array());
-
 $tournament1 = (object) array(
                               'numteams'=> 16,
                               'numleagues'=> 4,
@@ -48,7 +79,6 @@ $tournament1 = (object) array(
 $tournament2 = (object) array('tournament' => 'UNC basketball Intramurals 2016',
                               'tournament_id' => '1235',
                               'tournament_day' => '12-23-16');
-echo id;
 if($id == 1234){
     echo json_encode($tournament1);
 } else {
--- mockup/test.sh
+++ mockup/test.sh
@@ -1,8 +1,12 @@
 #curl POST -H 'Content-Type: application/json' -d '{"name": "New item", "year": "2009"}' https://wwwp.cs.unc.edu/Courses/comp426-f16/users/tgreer/final_project_milestones/mockup/makeTournament.php
 curl POST -H 'Content-Type: application/json' -d '{
+              "name": "yball horse", 
               "numteams": 16,
               "numleagues": 4,
+              "start_date" : "2013-12-3",
+              "end_date":"2013-12-4", 
+
               "in_bracket_play": true,
               "tournament_style": "round_robin",
-              "min_rest_time": "1:00",
-              "teams" :["Broncos", "Bulls", "Wingnuts", "Gamecocks", "Ampersands", "xss", "tar heels", "snoot boopers", "fly balls", "team chaos", "cow tippers", "crazy 8s", "ballers", "robots", "cat-people", "brainsuckers"];}' https://wwwp.cs.unc.edu/Courses/comp426-f16/users/tgreer/final_project_milestones/mockup/makeTournament.php
+              "min_rest_time": "1:00:00",
+              "teams" :["Broncos", "Bulls", "Wingnuts", "Gamecocks", "Ampersands", "xss", "tar heels", "snoot boopers", "fly balls", "team chaos", "cow tippers", "crazy 8s", "ballers", "robots", "cat-people", "brainsuckers"]}' https://wwwp.cs.unc.edu/Courses/comp426-f16/users/tgreer/final_project_milestones/mockup/makeTournament.php
