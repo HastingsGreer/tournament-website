2e8506edcdd57ded8132581607fc9e87ecc224bf
dharrell96 <dharrell96@gmail.com>
2016-12-09 06:51:07 -0500

    afas

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -11,12 +11,23 @@ $(document).ready(function(){
 		var numleagues=json.numleagues;
 		var teams = json.teams;
 		var games = json.games;
-		
+		var id2name = {}
+		for(var idx = 0; idx < teams.length; idx++){
+			id2name[teams[idx].team_id] = teams[idx];
+		}
 		for(var leagueidx=0; leagueidx< games.leagues.length; leagueidx++){
 			
 			for(var g=0; g<games.leagues[leagueidx].games.length; g++){
 				var game = games.leagues[leagueidx].games[g];
-				alert(game.gameID);
+				var t1=game.team1;
+				var t1name = id2name[t1].team_name;
+				var t2=game.team2;
+				var t2name=id2name[t2].team_name;
+				var t1score=game.team1Score;
+				var t2score=game.team2Score;
+
+				document.getElementById("pool_container").appendChild(game);
+				
 			}
 		}
 		
