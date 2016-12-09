f0f74f5c648512f0d9f2ff7bc147f3502521ce13
dharrell96 <dharrell96@gmail.com>
2016-12-09 01:07:27 -0500

    More changes

--- mockup/php/listTournament.php
+++ mockup/php/listTournament.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if (isset($_REQUEST['userid'])) {
 	$uid = $_REQUEST['userid'];
 
-	$result= $conn->query("SELECT * FROM users u, tournament t, user_tournament_junction ut WHERE ut.userID=u.id AND ut.tournamentID=t.id AND u.id=" . $uid);
+	$result= $conn->query("SELECT * (tournament) FROM users u, tournament t, user_tournament_junction ut WHERE ut.userID=u.id AND ut.tournamentID=t.id AND u.id=" . $uid);
 	if ($result->num_rows > 0) {
     // output data of each row
 		while($row = $result->fetch_assoc()) {
