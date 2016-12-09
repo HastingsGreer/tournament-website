854c5e23497d416aaebf43df861907be313c3770
dharrell96 <dharrell96@gmail.com>
2016-12-09 12:48:15 -0500

    adfadadfda

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -262,7 +262,7 @@ $('.game').on('click', function(e){
 	if(id != undefined){
 		var variables = id.split(" ");
 		alert(variables.length);
-		if(variables.lengh==3 && variables[2] !="" && variables[1] !="" && variable[0] !=""){
+		if(variables.lengh==3){
 			game=variables[0];
 			hname=id2name[variables[2]].team_name;
 			aname=id2name[variables[2]].team_name;
