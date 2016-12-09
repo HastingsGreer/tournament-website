cbc7d2b96a52d64761f6a032746d4cabead66a1c
dharrell96 <dharrell96@gmail.com>
2016-12-09 05:14:22 -0500

    Begin tourneyview

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-	
+	var url=window.location.split('?');
+	var id_string=url[1];
+	if(id_string==""){
+		window.location="tournament_view.html";
+	}
+	var link = 'gameTournament.php?id='+id_string;
+	alert(link);
+	//$.getJSON('gameTournament')
 
 });
