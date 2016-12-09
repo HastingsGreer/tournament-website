7949471e1f8946010172d425ec5838e07bcba032
dharrell96 <dharrell96@gmail.com>
2016-12-09 05:41:39 -0500

    Stuff

--- mockup/js/tournament_render.js
+++ mockup/js/tournament_render.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-	var url=window.location.split('?');
+	var url=String(window.location).split('?');
 	var id_string=url[1];
 	if(id_string==""){
 		window.location="tournament_view.html";
 	}
-	var link = 'gameTournament.php?id='+id_string;
-	alert(link);
-	//$.getJSON('gameTournament')
+	var link = 'gameTournament.php?'+id_string;
+	var result = $.getJSON(link, function(json){
+		alert(json);
+	});
 
 });
